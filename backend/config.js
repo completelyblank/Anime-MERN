0939b6521881f5a1105c4a215003e3b8690d20e8
Muhammad Raza <105001837+completelyblank@users.noreply.github.com>
2024-01-18 07:51:22 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL ="mongodb+srv://muhammadrkhan272:lyle12345678@blankdata.em7b5hz.mongodb.net/?retryWrites=true&w=majority";
+export const mongoDBURL ="mongodb+srv://<user>:<password>@blankdata.em7b5hz.mongodb.net/?retryWrites=true&w=majority";
